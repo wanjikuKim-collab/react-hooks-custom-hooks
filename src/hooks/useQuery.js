5af4a64793222baa8fbabbefcbad81bbc76b6408
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-19 09:58:00 +0300

    Completed lab

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -5,15 +5,26 @@ function useQuery(url) {
   const [isLoaded, setIsLoaded] = useState(false);
   // rename `posts` to a more generic `data`
   const [data, setData] = useState(null);
+  const [err,setErr] = useState(null);
 
   useEffect(() => {
     setIsLoaded(false);
     fetch(url)
-      .then((r) => r.json())
+      .then((r) =>{
+        if (r.status >= 400) {
+          throw new Error("Server responds with error!")
+      }
+      return r.json()
+          } )
       .then((data) => {
         setData(data);
         setIsLoaded(true);
-      });
+      },
+      err => {
+        setErr(err)
+        setIsLoaded(true)
+    }
+      );
   }, [url]);
   // the url is now a dependency
   // we want to use the side effect whenever the url changes
